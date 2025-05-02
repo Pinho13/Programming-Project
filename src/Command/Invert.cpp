57720177f057b56d24a65367b29fe7c9e8783b2b
Pinho13 <rafaelpinhotsilva@gmail.com>
2025-05-02 22:13:33 +0100

    Started implementating the Invert command

---
+++ src/Command/Invert.cpp
@@ -0,0 +1,38 @@
+#include "Command/Invert.hpp"
+#include "Image.hpp"
+#include "Color.hpp"
+#include <sstream>
+
+namespace prog {
+
+    namespace command {
+
+        Invert::Invert() : Command("Invert") {}
+
+        Invert::~Invert() {};
+
+        Image *Invert::apply(Image *img) {
+
+            // Cycles throught each pixel
+            for(int i = 0; i < img->width(); i++) {
+                for(int j = 0; j < img->height(); j++) {
+                    // Inverts colors
+                    Color &c_ = img->at(i, j);
+                    c_.red() = 255 - c_.red();
+                    c_.green() = 255 - c_.green();
+                    c_.blue() = 255 - c_.blue();
+                }
+            }
+            
+            return img;
+        }
+
+
+        std::string Invert::toString() const {
+            std::ostringstream ss;
+            ss << name();
+            return ss.str();
+        }
+
+    }
+}

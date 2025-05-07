a7331c971a77bb50a22b8e569af3e2361edc4f6b
Pinho13 <rafaelpinhotsilva@gmail.com>
2025-05-07 23:54:00 +0100

    Resize Implemented

---
+++ include/Command/Resize.hpp
@@ -0,0 +1,25 @@
+#ifndef PROJECT_RESIZE_HPP
+#define PROJECT_RESIZE_HPP
+
+#include "Command.hpp"
+#include <string>
+
+namespace prog {
+    namespace command {
+        class Resize : public Command {
+            public:
+                Resize(int x, int y, int w, int h);
+
+                ~Resize();
+
+                Image *apply(Image *img) override;
+
+                std::string toString() const override;
+            private:
+                int x_, y_, w_, h_;
+        };
+    }
+}
+
+
+#endif
---
+++ src/Command/Resize.cpp
@@ -0,0 +1,42 @@
+#include "Command/Resize.hpp"
+#include "Image.hpp"
+#include "Color.hpp"
+#include <sstream>
+
+namespace prog {
+
+    namespace command {
+
+        Resize::Resize(int x, int y, int w, int h) : Command("Resize"), x_(x), y_(y), w_(w), h_(h) {}
+
+        Resize::~Resize() {};
+
+        Image *Resize::apply(Image *img) {
+
+            // Creates a new image with the given dimensions
+            Image *new_img = new Image(w_, h_);
+            // Cycles throught each pixel of the new image
+            for(int i = 0; i < w_; i++) {
+                for(int j = 0; j < h_; j++) {
+                    // Avoids offbounds access
+                    if(img->width() <= i + x_ || img->height() <= j+y_) { break; }
+                    // Turns the pixel into the corresponding one of the old image
+                    Color &c_ = new_img->at(i, j);
+                    c_ = img->at(i + x_, j + y_);
+                }
+            }
+
+            // Dispose of given image
+            delete img;
+            return new_img;
+        }
+
+
+        std::string Resize::toString() const {
+            std::ostringstream ss;
+            ss << name() << " x:" << x_ << " y:" << y_ << " w:" << w_ << " h:" << h_;
+            return ss.str();
+        }
+
+    }
+}

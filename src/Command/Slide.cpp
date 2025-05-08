f20e423405cc336fa34f4991b47b8c86fdf451c1
Tomás Silva <vieiratomas.000@gmail.com>
2025-05-08 16:27:17 +0100

    FULL slide implementation

--- src/Command/Slide.cpp
+++ src/Command/Slide.cpp
@@ -11,20 +11,28 @@ namespace prog {
         Slide::~Slide() {};
 
         Image *Slide::apply(Image *img) {
-            // Cycle through each pixel
+            // Slide to the right
             for (int i = 0; i < img->width(); i++) {
                 for (int j = 0; j < img->height(); j++) {
                     Color& c_ = img->at(i, j);
-                    // Check if the current new pixel is within boundaries
-                    if (i + x_ < img->width() && j + y_ < img->height()) {
-                        Color& new_c = img->at(i + x_, j + y_);
+                    if (i + x_ < img->width()) {
+                        Color& new_c = img->at(i + x_, j);
                         Color temp = new_c;
                         new_c = c_;
                         c_ = temp;
                     }
-                    // Fix pixels that should be white
-                    if ((x_ && y_) && ((i < x_ && j >= y_) || (j < y_ && i >= x_))) {
-                        c_ = Color(255, 255, 255);
+                }
+            }
+
+            // Slide down
+            for (int i = 0; i < img->width(); i++) {
+                for (int j = 0; j < img->height(); j++) {
+                    Color& c_ = img->at(i, j);
+                    if (j + y_ < img->height()) {
+                        Color& new_c = img->at(i, j + y_);
+                        Color temp = new_c;
+                        new_c = c_;
+                        c_ = temp;
                     }
                 }
             }
